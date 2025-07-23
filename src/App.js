850d8928c9368935122b4a40402147d54a0e8ea4
evankahenya <evankahenya@gmail.com>
2025-07-23 23:17:15 +0300

    starter ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 export default function App(){
-  return<div>
+  return<div className="app">
+    <Logo/>
     <Form/>
+    <PackingList/>
+    <Stats />
   </div>
 }
-function Form(){
+function Logo(){
   return <h1>🌴Far Away🧳</h1>
 }
-function PackingList(){}
-function Stats(){}
+function Form(){
+  return <div className="add-form">
+    <h3>What do you need for you trip?</h3>
+  </div>
+}
+function PackingList(){
+  return <div className="list">List</div>
+}
+
+function Stats () { 
+  return <footer className="stats">
+    <em>💼 You have X items on your list, and you already packed X (x%)</em>
+    </footer>
+ }
